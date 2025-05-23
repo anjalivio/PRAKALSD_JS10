7f38ae711dadc1579d0d257170887c0f8b93c04d
anjalivio <anjaliviolita39@gmail.com>
2025-05-24 00:48:05 +0700

    praktikum 2 js 11

--- Tugas/Pertemuan12/SLLMain04.java
+++ Tugas/Pertemuan12/SLLMain04.java
@@ -1,24 +1,47 @@
 package Tugas.Pertemuan12;
+import java.util.Scanner;
 
 public class SLLMain04 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList04 sll = new SingleLinkedList04();
 
-        Mahasiswa04 mhs1 = new Mahasiswa04("21212203", "Dirga", "4D", 3.6);
-        Mahasiswa04 mhs2 = new Mahasiswa04("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa04 mhs3 = new Mahasiswa04("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa04 mhs4 = new Mahasiswa04("24212200", "Alvaro", "1A", 4.0);
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlah = sc.nextInt();
+        sc.nextLine(); 
 
-        sll.print(); // Kosong
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("\nData Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM     : ");
+            String nim = sc.nextLine();
+            System.out.print("Nama    : ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas   : ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK     : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
 
-        sll.addFirst(mhs1); // Dirga
+            Mahasiswa04 mhs = new Mahasiswa04(nim, nama, kelas, ipk);
+            if (i == 0) {
+                sll.addFirst(mhs);
+            } else {
+                sll.addLast(mhs);
+            }
+        }
+        System.out.println("\nDaftar Mahasiswa:");
         sll.print();
 
-        sll.addLast(mhs4); // Alvaro
-        sll.print();
+        System.out.println("data index 1 :");
+        sll.getData(1);
 
-        sll.insertAfter("Dirga", mhs3); // Cintia
-        sll.insertArt(2, mhs2); // Bimon
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf("bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
         sll.print();
     }
 }
